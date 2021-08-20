7b72ade18c02286799156c07f9d9a12efa002218
elena <elender1230@gmail.com>
2021-08-20 21:06:21 +0200

    master: add drawer background and change icons

--- app/src/main/java/com/ecirstea/creepyrabbit/ui/view/TestActivity.kt
+++ app/src/main/java/com/ecirstea/creepyrabbit/ui/view/TestActivity.kt
@@ -1,28 +1,20 @@
-package com.ecirstea.creepyrabbit.ui.navigation
+package com.ecirstea.creepyrabbit.ui.view
 
-import android.content.Context
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
-import com.ecirstea.creepyrabbit.MainActivity
 import com.ecirstea.creepyrabbit.R
-import com.ecirstea.creepyrabbit.utils.Constants
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.nav_header.*
-private const val TAG= "HomeActivity"
-class HomeActivity : AppCompatActivity() {
+
+class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
+        setContentView(R.layout.activity_test)
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
         val imgMenu = findViewById<ImageView>(R.id.imgMenu)
         val navView = findViewById<NavigationView>(R.id.navDrawer)
@@ -40,13 +32,5 @@ class HomeActivity : AppCompatActivity() {
                 textTitle.text = destination.label
             }
 
-        //Shared preferences
-        val sharedPreferences: SharedPreferences = applicationContext.getSharedPreferences(
-            Constants.SHARED_PREF_FILE,
-            Context.MODE_PRIVATE)
-        val prefUsername = sharedPreferences.getString("name_key","Anonymous")
-        val headerView : View = navView.getHeaderView(0)
-        val navUsername : TextView = headerView.findViewById(R.id.username)
-        navUsername.text = prefUsername
     }
 }
