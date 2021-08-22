2a03680025390f52a6f952d3185086d9a8950908
elena <elender1230@gmail.com>
2021-08-22 13:25:39 +0200

    master: add sign out button on navigation menu

--- app/src/main/java/com/ecirstea/creepyrabbit/ui/navigation/HomeActivity.kt
+++ app/src/main/java/com/ecirstea/creepyrabbit/ui/navigation/HomeActivity.kt
@@ -1,20 +1,19 @@
 package com.ecirstea.creepyrabbit.ui.navigation
 
-import android.content.Context
-import android.content.SharedPreferences
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.ecirstea.creepyrabbit.MainActivity
 import com.ecirstea.creepyrabbit.R
-import com.ecirstea.creepyrabbit.utils.Constants
 import com.ecirstea.creepyrabbit.utils.SharedApp
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.nav_header.*
@@ -38,6 +37,15 @@ class HomeActivity : AppCompatActivity() {
 
         navController
             .addOnDestinationChangedListener { _, destination, _ ->
+                if(destination.label?.equals("SignOut") == true){
+                    this.closeOptionsMenu()
+                    //SharedApp.prefs.token =""
+                    //SharedApp.prefs.name= ""
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        //putExtra(EXTRA_MESSAGE, message)
+                    }
+                    startActivity(intent)
+                }
                 textTitle.text = destination.label
             }
 
---
+++ app/src/main/java/com/ecirstea/creepyrabbit/ui/navigation/fragments/SignOutFragment.kt
@@ -0,0 +1,23 @@
+package com.ecirstea.creepyrabbit.ui.navigation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ecirstea.creepyrabbit.R
+
+
+class SignOutFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_sign_out, container, false)
+    }
+
+
+}
